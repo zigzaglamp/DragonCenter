98ab01f9d2880d93b8c7ddcf1ed8074a09ffaedc
m00nleaver <95206325+m00nleaver@users.noreply.github.com>
2021-11-30 01:34:57 +0900

    Add files via upload

---
+++ DragonCenter/src/com/center/script/dml/tblTeacher.sql
@@ -0,0 +1,20 @@
+--행 1
+insert into tblTeacher (teacher_seq, teacher_name, teacher_ssn, teacher_tel) values (teacher_seq.nextVal,'조인혜','990102-1178138','010-7447-8983');
+--행 2
+insert into tblTeacher (teacher_seq, teacher_name, teacher_ssn, teacher_tel) values (teacher_seq.nextVal,'문규빈','780326-2372669','010-4042-3999');
+--행 3
+insert into tblTeacher (teacher_seq, teacher_name, teacher_ssn, teacher_tel) values (teacher_seq.nextVal,'한우민','761224-1995236','010-6162-2161');
+--행 4
+insert into tblTeacher (teacher_seq, teacher_name, teacher_ssn, teacher_tel) values (teacher_seq.nextVal,'배지혁','780818-1298545','010-4925-8871');
+--행 5
+insert into tblTeacher (teacher_seq, teacher_name, teacher_ssn, teacher_tel) values (teacher_seq.nextVal,'예진욱','910721-1835166','010-9161-7778');
+--행 6
+insert into tblTeacher (teacher_seq, teacher_name, teacher_ssn, teacher_tel) values (teacher_seq.nextVal,'정은혁','940515-2535235','010-1116-1512');
+--행 7
+insert into tblTeacher (teacher_seq, teacher_name, teacher_ssn, teacher_tel) values (teacher_seq.nextVal,'황용희','880406-1888839','010-2928-2226');
+--행 8
+insert into tblTeacher (teacher_seq, teacher_name, teacher_ssn, teacher_tel) values (teacher_seq.nextVal,'유재웅','720811-2189193','010-4854-4900');
+--행 9
+insert into tblTeacher (teacher_seq, teacher_name, teacher_ssn, teacher_tel) values (teacher_seq.nextVal,'표영숙','970705-2682636','010-7553-3233');
+--행 10
+insert into tblTeacher (teacher_seq, teacher_name, teacher_ssn, teacher_tel) values (teacher_seq.nextVal,'설희재','740704-1446648','010-9322-5042');
