72104b3fedfd869ec4e5a0e7e6a29175913a0641
songseonghyun <57611077+songseonghyun@users.noreply.github.com>
2021-11-30 00:27:34 +0900

    Add files via upload

---
+++ DragonCenter/src/com/center/script/dml/tblCounselCategory.sql
@@ -0,0 +1,7 @@
+
+insert into tblCounselCategory(cc_seq, cc_content) values(cc_seq.nextVal, '공통');
+insert into tblCounselCategory(cc_seq, cc_content) values(cc_seq.nextVal, '국기직종훈련');
+insert into tblCounselCategory(cc_seq, cc_content) values(cc_seq.nextVal, '사업주위탁');
+insert into tblCounselCategory(cc_seq, cc_content) values(cc_seq.nextVal, '서문탁');
+insert into tblCounselCategory(cc_seq, cc_content) values(cc_seq.nextVal, '책상을탁');
+
