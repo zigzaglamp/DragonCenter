adca43d162fb94f9c89d6ebd093d70b592686c22
m00nleaver <95206325+m00nleaver@users.noreply.github.com>
2021-12-03 02:50:18 +0900

    Add files via upload

---
+++ DragonCenter/src/com/center/script/ddm/A.관리자/A-8_면접 관리.sql
@@ -0,0 +1,116 @@
+----------------------------------------------------------------------------------------------------------------------
+--A-8. 면접 관리 (관리자는 비회원의 면접을 관리)
+----------------------------------------------------------------------------------------------------------------------
+--A-8-1. 비회원의 면접 신청 정보를 조회한다.
+----------------------------------------------------------------------------------------------------------------------
+select 
+        seq as "면접번호",
+        name as "이름",
+        tel as "전화번호",
+        oc_seq as "희망과정",
+        hopedate as "희망면접일",
+        ensuredate as "확정면접일",
+        result as "면접결과"
+from vwinterviewinfo order by seq; --seq순 조회
+
+select 
+        seq as "면접번호",
+        name as "이름",
+        tel as "전화번호",
+        oc_seq as "희망과정",
+        hopedate as "희망면접일",
+        ensuredate as "확정면접일",
+        result as "면접결과"
+from vwinterviewinfo 
+    where hopedate between to_date('19-01-01', 'yy-mm-dd') and to_date('19-12-31', 'yy-mm-dd')--년도별 조회
+order by seq, hopedate;
+
+----------------------------------------------------------------------------------------------------------------------
+-- A-8-2.면접 신청 일자와 가능일자를 비교하여 면접 확정일을 입력한다.
+----------------------------------------------------------------------------------------------------------------------
+
+--면접 일자 없는 것만 뽑아오기
+select * from vwInterviewNull;
+
+
+--이번 달 희망 면접일//면접 가능일자 확인하기
+--vwThisMonthHopeDate로 확인
+select v.regdate, v.interviewer_name,v.interviewer_seq from vwThisMonthHopeDate v left outer join tblinterview i on v.interviewer_seq = i.interviewer_seq;
+ 
+ 
+ --희망면접일이 공휴일이 아니라 면접이 가능한 학생 번호 
+ select tm.regdate, n."면접번호" from vwthismonthhopedate tm left join  vwinterviewnull n on tm.interviewer_seq = n."면접번호"
+where n."확정 면접일" is null and instr(tm.interviewer_name,'*')=0;
+
+
+--면접일이 공휴일+주말이라 안되는 신청자
+select tm.regdate, n."면접번호" from vwthismonthhopedate tm inner join  vwinterviewnull n on tm.interviewer_seq = n."면접번호"
+where n."확정 면접일" is null and instr(tm.interviewer_name,'*')>0;
+
+
+--면접 확정일자 입력
+insert into tblInterview(interviewer_seq, interview_date, interview_result) 
+    values(803, 
+    --'2019-01-06', --임의 입력
+    (select interviewer_date from tblInterviewer where interviewer_seq = 803),--희망날짜 = 면접날짜
+    null);
+
+
+--평일인 경우 확정일자 반복해서 입력
+--프로시저 loop 이용
+
+----------------------------------------------------------------------------------------------------------------------
+-- A-8-3. 면접 응시 완료자들은 면접 합격 여부를 입력한다.
+----------------------------------------------------------------------------------------------------------------------
+UPDATE tblInterview 
+    SET interview_result = 'Y' 
+    --SET interview_result = 'N' 불합격 
+    WHERE to_char(interview_date) =to_char(sysdate, 'yy/mm/dd')--오늘 날짜
+    --WHERE to_char(interview_date) =to_char('21/10/30', 'yy/mm/dd')-- 날짜 선택
+    and interviewer_seq = 802;--면접자 번호
+    --관리자면 관리자 seq입력하면 됨
+
+
+
+
+----------------------------------------------------------------------------------------------------------------------
+-- A-8-4.면접 합격자의 수강 과정은 희망 과정을 참고하며, 변경할 수 있다.
+----------------------------------------------------------------------------------------------------------------------
+--면접 합격자 교육생에 넣기 (희망과정에 정원이 있을 경우)
+--뷰 사용
+--현재 개설예정 강의
+select * from vwcourseinfo where oc_startdate>sysdate ;
+
+select 
+    o.oc_seq, s.course_name , count(*) as cnt
+from vwstudentinfo s 
+    left join tblenrollment e
+        on s.enrollment_seq = e.enrollment_seq
+            inner join tblopencourse o
+                on e.oc_seq = o.oc_seq
+where o.oc_startdate>sysdate
+group by o.oc_seq, s.course_name order by count(*) desc;
+
+
+
+
+--개설 예정 과정이 정원 초과인지 아닌지 확인하기
+--프로시저 이용
+
+
+--과정 수기 입력
+--뷰사용
+insert into tblStudent (student_seq, student_name, student_ssn, student_tel, student_coursenum) 
+     values (student_seq.nextVal,
+        (select name from vwinterviewinfo where result = 'Y' and seq = 300),--면접자 번호 수기 입력
+        '951219-0000001',--수기 입력
+        (select tel from vwinterviewinfo where result = 'Y' and seq = 300),--면접자 번호 수기 입력
+         42--면접자 번호 수기 입력
+                );
+
+
+
+--면접 합격자 수강신청에 넣기 
+insert into tblEnrollment (enrollment_seq, oc_seq, student_seq) 
+    values (enrollment_seq.nextVal,과정번호,학생번호);
+
---
+++ DragonCenter/src/com/center/script/ddm/A.관리자/A-9_사후 처리.sql
@@ -0,0 +1,111 @@
+----------------------------------------------------------------------------------------------------------------------
+--A-9. 사후 처리
+--(교육생에 대한 취업 지원 및 고용보험 가입(취업) 확인)
+--B-7. 사후 처리 조회
+--(교육생에 대한 취업 지원 및 고용보험 가입 확인)
+----------------------------------------------------------------------------------------------------------------------
+--A-9-1 교육생 정보와 취업 여부, 취업지원 희망 여부를 입력 및 조회한다.
+--B-7-1 교육생 정보와 취업 여부, 취업지원 희망 여부를 입력 및 조회한다.
+----------------------------------------------------------------------------------------------------------------------
+
+  --사후 처리 기재된 전체 교육생
+  select 
+          p.pm_seq as "사후관리 여부",
+          v.sname as " 학생명", 
+          v.stel as "전화번호", 
+          p.pm_employee as "취업여부", 
+          p.pm_jobseeker as "취업지원 희망 여부" 
+  from vwstudentinfo v 
+    inner join tblpostmanagement p
+        on v.enrollment_seq = p.enrollment_seq
+            inner join tbljobseeker j
+                on p.pm_seq = j.pm_seq;
+
+--그 중 취업한 사람
+  select 
+          p.pm_seq as "사후관리 여부",
+          v.sname as " 학생명", 
+          v.stel as "전화번호", 
+          p.pm_employee as "취업여부", 
+          p.pm_jobseeker as "취업지원 희망 여부" 
+  from vwstudentinfo v 
+    inner join tblpostmanagement p
+        on v.enrollment_seq = p.enrollment_seq
+            inner join tbljobseeker j
+                on p.pm_seq = j.pm_seq
+                where p.pm_employee = 'Y'
+                ;
+----------------------------------------------------------------------------------------------------------------------
+--A-9-2 취업자의 경우 취업분야, 취업일, 고용보험 가입 여부 확인 가능
+--B-7-2 취업자의 경우 취업분야, 취업일, 고용보험 가입 여부 확인 가능 
+----------------------------------------------------------------------------------------------------------------------
+ select 
+          v.sname as " 학생명", 
+          v.stel as "전화번호", 
+          --p.pm_employee as "취업여부",
+          e.employee_hiredate as "취업일",
+          e.employee_insurance as"고용 보험 가입 여부",
+          rc.rc_name as "취업 분야"
+  from vwstudentinfo v 
+    inner join tblpostmanagement p
+        on v.enrollment_seq = p.enrollment_seq
+            inner join tblemployee e
+                on p.pm_seq = e.pm_seq
+                    inner join tblrecruitcategory rc
+                        on rc.rc_seq = e.rc_seq
+                        where p.pm_employee = 'Y';
+
+
+----------------------------------------------------------------------------------------------------------------------
+--A-9-3  취업 지원 희망자의 경우 희망 연봉, 근무지, 희망 분야를 확인 
+--B-7-3 취업 지원 희망자의 경우 희망 연봉, 근무지, 희망 분야를 확인
+----------------------------------------------------------------------------------------------------------------------
+
+select 
+          p.pm_seq as "사후관리 여부",
+          v.sname as " 학생명", 
+          v.stel as "전화번호", 
+          j.js_salary as "희망연봉",
+          a.area_name as "희망근무지",
+          rc.rc_name as "희망취업분야",
+          --p.pm_jobseeker as "취업지원 희망 여부",
+          p.pm_employee as "취업여부"
+           
+  from vwstudentinfo v 
+    inner join tblpostmanagement p
+        on v.enrollment_seq = p.enrollment_seq
+            inner join tblemployee e
+                on p.pm_seq = e.pm_seq
+                    inner join tblrecruitcategory rc
+                        on rc.rc_seq = e.rc_seq
+                            inner join tbljobseeker j
+                                on p.pm_seq = j.pm_seq
+                                    inner join tblarea a
+                                        on j.area_seq = a.area_seq
+                                where p.pm_jobseeker ='Y';
+
+----------------------------------------------------------------------------------------------------------------------
+--A-9-4  알선하는 기업의 희망 근무자와 일치하는 교육생에게 연결한다.
+--B-7-4 알선하는 기업의 희망 근무자와 일치하는 교육생에게 연결한다.
+----------------------------------------------------------------------------------------------------------------------
+select 
+        s.sname as "학생명", 
+        s.stel as "학생번호",
+        a.area_name as "지역명",
+        c.company_name as "회사명",
+        c.company_tel as "회사번호"
+from tblRecruitInfo r
+    inner join tblJobSeeker j
+        on j.rc_seq = r.rc_seq
+        and r.ri_salary = j.js_salary --연봉
+                inner join tblcompany c
+                    on r.company_seq = c.company_seq
+                        inner join tblarea a
+                            on c.area_seq = a.area_seq
+                                inner join tblpostmanagement p
+                                    on p.pm_seq = j.pm_seq 
+                                        inner join vwstudentinfo s
+                                            on p.enrollment_seq =s.enrollment_seq;
+
+
+
