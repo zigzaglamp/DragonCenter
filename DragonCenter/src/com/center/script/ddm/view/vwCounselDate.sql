f9585293a624ffe3490c55f989c2d41743348d8c
songseonghyun <57611077+songseonghyun@users.noreply.github.com>
2021-12-02 15:59:32 +0900

    Create vwCounselDate.sql

---
+++ DragonCenter/src/com/center/script/ddm/view/vwCounselDate.sql
@@ -0,0 +1,15 @@
+-- counseling distributed by date
+CREATE OR REPLACE VIEW vwCounselDate
+AS
+SELECT 
+	cl.cl_date,
+	c.course_name,
+	s.student_name
+FROM tblCourse c
+	INNER JOIN tblOpenCourse oc ON c.course_seq = oc.course_seq
+		INNER JOIN tblEnrollment e ON oc.OC_SEQ = e.OC_SEQ
+			INNER JOIN  tblStudent s ON e.student_seq = s.student_seq
+				INNER JOIN tblQuestion q ON s.student_seq = q.student_seq
+					INNER JOIN tblAnswer a ON  q.question_seq = a.question_seq
+						INNER JOIN tblTeacher t ON a.teacher_seq = t.teacher_seq
+							INNER JOIN tblCounselLog cl ON cl.enrollment_seq = e.enrollment_seq;
