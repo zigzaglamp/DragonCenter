e78bacae565226e31fb5173f5a9af09e24d596c0
umingming <u_0112@naver.com>
2021-12-02 10:50:26 +0900

    test

--- DragonCenter/src/com/center/script/dml/data/tblAdmin.sql
+++
@@ -1,8 +0,0 @@
--- 관리자
-
-
--- 실제, 더미
-INSERT INTO tblAdmin(admin_seq, admin_name, admin_ssn, admin_tel) VALUES (1, '김첨지', '751023-1930294', '010-4373-8503');
-INSERT INTO tblAdmin(admin_seq, admin_name, admin_ssn, admin_tel) VALUES (2, '유병학', '851103-1655846', '010-9209-7394');
-INSERT INTO tblAdmin(admin_seq, admin_name, admin_ssn, admin_tel) VALUES (3, '김동경', '830527-2132444', '010-5284-6127');
-INSERT INTO tblAdmin(admin_seq, admin_name, admin_ssn, admin_tel) VALUES (4, '최석', '900613-1748924','010-1073-7190');
