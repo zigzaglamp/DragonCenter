313897519123684bcafc99a7cf8d91abd52a84dd
m00nleaver <95206325+m00nleaver@users.noreply.github.com>
2021-12-03 11:55:07 +0900

    Add files via upload

--- DragonCenter/src/com/center/script/ddm/view/vwStudentIfo.sql
+++ DragonCenter/src/com/center/script/ddm/view/vwStudentIfo.sql
@@ -4,10 +4,14 @@ create or replace view vwStudentInfo
 as
 select 
         s.student_name as sname, 
+        s.student_seq as sseq,
         s.student_tel as stel, 
         substr(s.student_ssn,-7) as sssn,
         c.course_name as ccourse_name,
-        e.enrollment_seq as eseq
+        c.course_seq as ccourse_seq,
+        e.enrollment_seq as eseq,
+        o.oc_startdate as startdate,
+        o.oc_enddate as enddate
 from tblenrollment e
     inner join tblstudent s
         on e.student_seq = s.student_seq
@@ -15,5 +19,5 @@ from tblenrollment e
                 on e.oc_seq = o.oc_seq
                     inner join tblcourse c
                         on o.course_seq = c.course_seq;
-                        
-select * from vwStudentInfo;      
+ commit;                       
+select * from vwStudentInfo order by sseq; 
