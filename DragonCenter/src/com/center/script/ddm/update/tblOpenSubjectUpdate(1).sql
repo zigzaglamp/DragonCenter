1bc750b959743b9a74dc220cd94da5aa71eb3626
zigzaglamp <93372099+zigzaglamp@users.noreply.github.com>
2021-12-03 13:49:55 +0900

    Add files via upload

---
+++ DragonCenter/src/com/center/script/ddm/update/tblOpenSubjectUpdate(1).sql
@@ -0,0 +1,6 @@
+update tblopensubject set os_startdate = '19-02-01', os_enddate = '19-03-14' where os_seq = 1;
+update tblopensubject set os_startdate = '19-03-15', os_enddate = '19-05-01' where os_seq = 2;
+update tblopensubject set os_startdate = '19-05-02', os_enddate = '19-06-01' where os_seq = 3;
+update tblopensubject set os_startdate = '19-06-02', os_enddate = '19-07-01' where os_seq = 4;
+update tblopensubject set os_startdate = '19-07-02', os_enddate = '19-08-02' where os_seq = 29;
+update tblopensubject set os_startdate = '19-08-03', os_enddate = '19-09-01' where os_seq = 32;
