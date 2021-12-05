9b59145f960b243fa991b4160e1276af4daf04f2
zigzaglamp <93372099+zigzaglamp@users.noreply.github.com>
2021-12-05 19:58:18 +0900

    Add files via upload

--- DragonCenter/src/com/center/script/ddm/A.관리자/A-4.개설 과목 관리.sql
+++ DragonCenter/src/com/center/script/ddm/A.관리자/A-4.개설 과목 관리.sql
@@ -1,12 +1,15 @@
+--------------------------------------------------------------------------------
 -- A-4. 개설 과목 관리
 -- A-4.1) 개설과목 입력/수정/삭제
+--------------------------------------------------------------------------------
 insert into tblOpenSubject (os_seq, subject_seq, oc_seq, os_startdate, os_enddate)
        values ((select max(os_seq)+1 from tblopensubject), 52, 13, '20-10-24', '20-11-30');
 update tblopensubject set subject_seq = 56 where os_seq = 0;
 update tblopensubject set oc_seq = 5 where os_seq = 0;
 delete from tblopensubject where os_seq = 0;
-
+--------------------------------------------------------------------------------
 -- A-4.2) 개설 과목 정보 출력
+--------------------------------------------------------------------------------
 select
     distinct s.subject_name as "과목명",
     os.os_startdate || '~' || os.os_enddate as "기간",
@@ -22,20 +25,26 @@ from tblsubject s
        inner join tblteachermanagement tm
         on oc.oc_seq = tm.oc_seq
          inner join tblteacher t
-          on tm.teacher_seq = t.teacher_seq;
+          on tm.teacher_seq = t.teacher_seq
+           order by s.subject_name;
+--------------------------------------------------------------------------------
 -- A-4.3) 교재 정보 입력/출력/수정/삭제
+--------------------------------------------------------------------------------
 insert into tblbook (book_seq, book_name, book_publisher, book_price, subject_seq)
        values ((select max(book_seq)+1 from tblbook), '함께하는 오라클', 쌍용북스, 15000, 3);
 update tblbook set book_name where subject_seq = 3;
 update tblbook set book_publisher where subject_seq = 3;
 update tblbook set book_price where subject_seq = 3;
 delete from tblbook where subject_seq = 3;
+--------------------------------------------------------------------------------
+
+--procedure
 
 ------------------------------개설 과목 입력------------------------------------
 begin
 procNewSubject(60,5,'20-10-24','20-11-30');
 end; -- 기초과목번호 subject_seq, 소속개설과정번호 oc_seq
--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 create or replace procedure procNewSubject(
     psub1 number,
     poc1 number,
@@ -63,6 +72,7 @@ begin
        posdate2 <= posdate2check then
         insert into tblOpenSubject (os_seq, subject_seq, oc_seq, os_startdate, os_enddate)
                values ((select max(os_seq)+1 from tblopensubject), psub1, poc1, posdate1, posdate2);
+       dbms_output.put_line('해당개설과정이 성공적으로 등록되었습니다.');
     else
     dbms_output.put_line('해당개설과정의 날짜범위를 벗어났습니다.');
     end if;
@@ -72,3 +82,51 @@ exception
     poc1check := 0;
     dbms_output.put_line('유효하지않는값존재');
 end procNewsubject;
+------------------------------개설 과목 출력------------------------------------
+begin
+procOpenSubjectOutput(10);
+end; -- 개설과목번호 os_seq(29...33해당os_seq미존재:기초과목테이블에는있음)
+-------------------------------------------------------------------------------
+-- 과목명, 과목기간(시작 년월일, 끝 년월일), 교재명, 교사명
+-------------------------------------------------------------------------------
+create or replace procedure procOpenSubjectOutput(
+    pos1 number
+)
+is
+    vrow tblOpenSubject%rowtype;
+    psub_seq number;
+    psub_name varchar2(60);
+    pteacher_name varchar2(30);
+    pbook_name varchar2(100);
+begin
+    select * into vrow from tblopensubject where os_seq = pos1;
+    select subject_seq into psub_seq from tblopensubject where os_seq = pos1;
+    select subject_name into psub_name from tblsubject where subject_seq = psub_seq;
+    select t.teacher_name into pteacher_name from tblteacher t where teacher_seq
+    = (select tm.teacher_seq from tblteachermanagement tm where oc_seq
+    = (select os.oc_seq from tblopensubject os where os_seq = pos1));
+    select book_name into pbook_name from tblbook where subject_seq = pos1;
+    dbms_output.put_line('개설과목명: ' || psub_name);
+    dbms_output.put_line('시작년월일: ' || vrow.os_startdate);
+    dbms_output.put_line('종료년월일: ' || vrow.os_enddate);
+    dbms_output.put_line('교사명: ' || pteacher_name);
+    dbms_output.put_line('교재명: ' || pbook_name);
+exception
+    when NO_DATA_FOUND then
+    dbms_output.put_line('해당os_seq가 존재하지않습니다');
+end procOpenSubjectOutput;
+/*
+개설과목과 기초과목테이블 1:1매칭아님
+교사가 os가 아닌 oc별로 할당되어있음
+select * from tblteacher;
+select * from tblteachermanagement where oc_seq = 1;
+select * from tblopencourse;
+select * from tblcourse;
+select * from tblopensubject;
+select * from tblbook;
+select * from tblsubject;
+select * from tblstudent;
+select * from tblenrollment;
+select subject_seq,count(subject_seq) from tblbook group by subject_Seq; 
+*/
+--------------------------------------------------------------------------------
--- DragonCenter/src/com/center/script/ddm/A.관리자/A-6.시험 관리 및 성적 조회.sql
+++ DragonCenter/src/com/center/script/ddm/A.관리자/A-6.시험 관리 및 성적 조회.sql
@@ -1,5 +1,7 @@
+--------------------------------------------------------------------------------
 -- A-6. 시험 관리 및 성적 조회
 -- A-6.1) 개설 과목 별로 성적 등록 여부, 시험 문제 파일 등록 여부
+--------------------------------------------------------------------------------
 select
     os.os_seq as "개설과목번호",
     s.score_exam as "필기성적",
@@ -12,7 +14,24 @@ from tblopensubject os
    inner join tbltest t
     on os.oc_seq = t.os_seq
      order by os.os_seq;
+     select * from tblopencourse
+     select * from tblcourse;
+     select * from tblsubject where subject_seq in (1,2,3,4,29,32);
+--------------------------------------------------------------------------------
 -- A-6.2) 교육생 개인이 수강한 모든 개설 과목에 대한 성적 정보(개설 과목명, 개설 과목 기간, 교사명, 출결, 필기, 실기) 출력
+--------------------------------------------------------------------------------
+update tblopensubject set os_startdate = '19-02-01', os_enddate = '19-03-14' where os_seq = 1;
+update tblopensubject set os_startdate = '19-03-15', os_enddate = '19-05-01' where os_seq = 2;
+update tblopensubject set os_startdate = '19-05-02', os_enddate = '19-06-01' where os_seq = 3;
+update tblopensubject set os_startdate = '19-06-02', os_enddate = '19-07-01' where os_seq = 4;
+update tblopensubject set os_startdate = '19-07-02', os_enddate = '19-08-02' where os_seq = 29;
+update tblopensubject set os_startdate = '19-08-03', os_enddate = '19-09-01' where os_seq = 32;
+/*
+tblscore 인설트
+tblteachermanagement 인설트
+tblopensubject os_seq 1 6개 1,2,3,4,29,32
+tblopensubject 기간 업데이트
+*/
 select
     distinct sj.subject_name as "개설과목명",
     os.os_startdate as "시작일",
@@ -33,12 +52,14 @@ from tblstudent s
          inner join tblteachermanagement tm
           on oc.oc_seq = tm.oc_seq
            inner join tblscore sc
-            on os.oc_seq = sc.os_seq
+            on os.os_seq = sc.os_seq
              inner join tblteacher t
               on tm.teacher_seq = t.teacher_seq
                where e.student_seq = 3
                 order by os.os_startdate;
+-------------------------------------------------------------------------------
 -- A-6.3) 개설 과목별 성적 출력(과정명, 개설과정기간, 강의실명, 개설과목명, 교사명, 교육생 이름, 교육생주민번호, 필기, 실기 출력                                                                
+-------------------------------------------------------------------------------
 select
 distinct c.course_name as "과정명",
     c.course_period as "개설과정기간",
@@ -68,3 +89,90 @@ from tblopencourse oc
                 on st.student_seq = e.student_seq
                  inner join tblscore sc
                   on sc.enrollment_seq = e.enrollment_seq;
+--------------------------------------------------------------------------------
+
+--procedure
+
+--------------------------------------------------------------------------------
+/*
+A-6. 시험 관리 및 성적 조회
+2) 교육생 개인이 수강한 모든 개설 과목에 대한 성적 정보(개설 과목명, 개설 과목 시작일, 개설 과목 종료일, 필기성적, 실기성적, 출결성적, 교사명) 출력
+*/
+-------------------------------------------------------------------------------
+begin
+    procStudentScoreInfo(3);
+end;
+-- 수강신청번호
+-------------------------------------------------------------------------------
+create or replace procedure procStudentScoreInfo(
+    pes1 number
+)
+is
+    cursor vcursor
+        is select
+        "개설과목명",
+        "시작일",
+        "종료일",
+        "필기성적",
+        "실기성적",
+        "출결성적",
+        "교사명",
+        "a",
+        "b",
+        "c"
+        from vwStudentScoreInfo where "수강번호" = pes1;
+        
+        vexam_result number;
+        vskill_result number;
+        vattendance_result number;
+begin
+    for vrow in vcursor loop
+        
+        vexam_result := vrow."a" * (vrow."필기성적"/50);
+        vskill_result := vrow."b" * (vrow."실기성적"/50);
+        vattendance_result := vrow."c" * (vrow."출결성적"/50);
+        dbms_output.put_line(
+        '['||vrow."개설과목명"||']'||'   '||
+        '['||vrow."시작일"||']'||'   '||
+        '['||vrow."종료일"||']'||'   '||
+        '['||vexam_result||']'||'   '||
+        '['||vskill_result||']'||'   '||
+        '['||vattendance_result||']'||'   '||
+        '['||vrow."교사명"||']');
+    
+    end loop;
+end;
+-------------------------------------------------------------------------------
+create or replace view vwStudentScoreInfo
+as select
+    distinct sj.subject_name as "개설과목명",
+    os.os_startdate as "시작일",
+    os.os_enddate as "종료일",
+    sc.score_exam as "필기성적",
+    sc.score_skill as "실기성적",
+    sc.score_attendance as "출결성적",
+    t.teacher_name as "교사명",
+    e.enrollment_seq as "수강번호",
+    sp.exam_per as "a",
+    sp.skill_per as "b",
+    sp.attendance_per as "c",
+    os.os_seq
+from tblstudent s
+ inner join tblenrollment e
+  on s.student_seq = e.student_seq
+   inner join tblopencourse oc
+    on e.oc_seq = oc.oc_seq
+     inner join tblopensubject os
+      on oc.oc_seq = os.oc_seq
+       inner join tblsubject sj
+        on sj.subject_seq = os.subject_seq
+         inner join tblteachermanagement tm
+          on oc.oc_seq = tm.oc_seq
+           inner join tblscore sc
+            on os.os_seq = sc.os_seq
+             inner join tblteacher t
+              on tm.teacher_seq = t.teacher_seq
+               inner join tblscorepercent sp
+                on sp.os_seq = os.os_seq
+                 order by os.os_startdate
+-------------------------------------------------------------------------------
