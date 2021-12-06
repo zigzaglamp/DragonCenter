d15e68182c3ce6ea00bdcfd009f3e2d97235c7ee
LEE YUMI <87955005+umingming@users.noreply.github.com>
2021-12-06 17:43:51 +0900

    Update final_presentation.sql

--- DragonCenter/src/com/center/script/ddm/발표 취합본/final_presentation.sql
+++ DragonCenter/src/com/center/script/ddm/발표 취합본/final_presentation.sql
@@ -5,37 +5,6 @@
 --------------------------------------------------------------------------------
 -- 1. 과정 정보 조회
 --------------------------------------------------------------------------------
--- 개설 과정 뷰
---------------------------------------------------------------------------------
-create or replace view vwOpenCourse
-as
-select
-    toc.oc_seq as oc_seq,
-    tc.course_name as course_name,
-    toc.oc_startdate as oc_startdate,
-    toc.oc_enddate as oc_enddate,
-    tr.room_seq as room_seq,
-    tr.room_name as room_name,
-    tr.room_capacity as room_capacity,
-    case
-        when (select count(*) from tblOpenSubject 
-              group by oc_seq having oc_seq = toc.oc_seq) > 0 then 'Y'
-        else 'N'
-    end as regsub,
-    (select 
-         case count(*)
-             when 1 then 0
-             else count(*)
-         end
-     from tblEnrollment e full outer join tblOpenCourse oc
-         on (e.oc_seq = oc.oc_seq)
-     group by oc.oc_seq having oc.oc_seq = toc.oc_seq) as num
-from tblOpenCourse toc inner join tblCourse tc
-    on (toc.course_seq = tc.course_seq) inner join tblRoom tr
-    on (toc.room_seq = tr.room_seq) 
-order by toc.oc_startdate desc;
-
---------------------------------------------------------------------------------
 -- 과정 조회 프로시저
 --------------------------------------------------------------------------------
 create or replace procedure procGetCourse(
@@ -227,6 +196,37 @@ end;
 -- A-3.1) 개설 과정 정보 관리(tblOpenCourse)
 --------------------------------------------------------------------------------
 -- 1. 개설 과정 조회
+--------------------------------------------------------------------------------
+-- 개설 과정 뷰
+--------------------------------------------------------------------------------
+create or replace view vwOpenCourse
+as
+select
+    toc.oc_seq as oc_seq,
+    tc.course_name as course_name,
+    toc.oc_startdate as oc_startdate,
+    toc.oc_enddate as oc_enddate,
+    tr.room_seq as room_seq,
+    tr.room_name as room_name,
+    tr.room_capacity as room_capacity,
+    case
+        when (select count(*) from tblOpenSubject 
+              group by oc_seq having oc_seq = toc.oc_seq) > 0 then 'Y'
+        else 'N'
+    end as regsub,
+    (select 
+         case count(*)
+             when 1 then 0
+             else count(*)
+         end
+     from tblEnrollment e full outer join tblOpenCourse oc
+         on (e.oc_seq = oc.oc_seq)
+     group by oc.oc_seq having oc.oc_seq = toc.oc_seq) as num
+from tblOpenCourse toc inner join tblCourse tc
+    on (toc.course_seq = tc.course_seq) inner join tblRoom tr
+    on (toc.room_seq = tr.room_seq) 
+order by toc.oc_startdate desc;
+
 --------------------------------------------------------------------------------
 -- 개설 과정 조회 프로시저
 --------------------------------------------------------------------------------
